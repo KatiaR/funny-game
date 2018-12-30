4fe88cc0e142f73a65da38615e1cc052bca6777c
Katsiaryna Ramaniuk <katia-l@mail.ru>
2018-12-31 02:19:42 +0300

    feat: add animation hero to the battlefield

--- game/src/screens/hero/hero-template.js
+++ game/src/screens/hero/hero-template.js
@@ -1,4 +1,17 @@
 export default `
-
-
+<div class="hero">
+  <div class="hero-body">
+    <img src="./screens/hero/images/bodyHero.png" alt="hero-body" />
+    <div class="hero-armTop">
+      <img  src="./screens/hero/images/armTop.png" alt="hero-arm" />
+    </div>
+    <div class="hero-armBottom">
+      <img src="./screens/hero/images/armHero.png" alt="hero-arm" />
+    </div>
+    
+    <div class="hero-leg">
+      <img src="./screens/hero/images/leftLeg.png" alt="hero-leg" />
+    </div>
+  </div>
+</div>
 `;
